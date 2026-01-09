4e7a9976852825aef805fc47fe867268a8f0bf6b
MTarique51 <mohdtarique7223@gmail.com>
2026-01-09 11:38:06 -0500

    first Commit

---
+++ JavaBasics.java
@@ -0,0 +1,37 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");      
+        System.out.println("Hello World!");  // (ln)&(\n) --> for next line 
+        System.out.println("Hello World!");
+
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("* ");
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Tony Stark";
+        System.out.println(name);
+
+        a = b;
+        System.out.println(a);
+
+        char ch ='a';
+        System.out.println(ch);
+
+        boolean var = true;
+        System.out.println(var);
+        double price = 100000.5;
+        int num = 25;
+        // log
+        //float
+        short n =240;
+
+        int sum = a + b;
+        System.out.println(sum);                                /*sum = 10 */
+        
+    }
+}

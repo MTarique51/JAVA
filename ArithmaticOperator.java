16abd96c4e314438e04bfca88a63534c0f959a81
MTarique51 <mohdtarique7223@gmail.com>
2026-01-23 04:09:25 -0500

    Operators(Arithmatic)

---
+++ ArithmaticOperator.java
@@ -0,0 +1,18 @@
+public class ArithmaticOperator {
+        public static void main(String[] args) {
+            int A = 10;
+            int B = 5;
+            System.out.println("Add = " + (A+B));   // Binary ArithmaticOperator//
+
+            int C = 100;
+            int D = ++C;
+            System.out.println(C);
+            System.out.println(D);
+
+            int E = 100;
+            int F = E++;
+            System.out.println(E);
+            System.out.println(F);
+        }
+    
+}

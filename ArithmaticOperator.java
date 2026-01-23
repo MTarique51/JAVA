0c0af2ed4018f0292ee1b2ea1b515dce33802922
MTarique51 <mohdtarique7223@gmail.com>
2026-01-23 04:14:28 -0500

    modified

--- ArithmaticOperator.java
+++ ArithmaticOperator.java
@@ -4,14 +4,15 @@ public class ArithmaticOperator {
             int B = 5;
             System.out.println("Add = " + (A+B));   // Binary ArithmaticOperator//
 
-            int C = 100;
+            // Unary (Arithmatic Operator)//
+            int C = 100;                            
             int D = ++C;
-            System.out.println(C);
+            System.out.println(C);                  // pre Increment // 
             System.out.println(D);
 
             int E = 100;
             int F = E++;
-            System.out.println(E);
+            System.out.println(E);                   // Post Incement //
             System.out.println(F);
         }
     

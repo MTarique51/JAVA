e72f5cf2e0f6fd2e876602a6ccc35928bfcf3ea7
MTarique51 <mohdtarique7223@gmail.com>
2026-02-05 06:46:11 -0500

    Print_Number

---
+++ Loops/Print_Number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Print_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int N = sc.nextInt();
+
+        int count = 1;
+
+        while (count<=N) {
+            System.out.print(count+" ");
+            count++;            
+        }
+    }
+}

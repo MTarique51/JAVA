86c5cc3c1ac453383a462bfaa26243256f4f885a
MTarique51 <mohdtarique7223@gmail.com>
2026-02-07 07:06:51 -0500

    Function

---
+++ Functions OR Methods/Function_Demo.java
@@ -0,0 +1,11 @@
+public class Function_Demo {
+    public static void PrintMyName() {
+        System.out.println("Mohammad Saquib");
+        System.out.println("Mohammad Sajid");
+        System.out.println("Mohammad Tarique");
+        System.out.println("Ahmad Ali");
+    }
+    public static void main(String[] args) {
+        PrintMyName();
+    }
+}

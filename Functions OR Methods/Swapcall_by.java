bfa97e79e77a74ead8206566d66df253a415557d
MTarique51 <mohdtarique7223@gmail.com>
2026-02-09 05:50:43 -0500

    Swapping (Call By Value

---
+++ Functions OR Methods/Swapcall_by.java
@@ -0,0 +1,25 @@
+public class Swapcall_by {
+    public static void main(String[] args) {
+        
+        // SWAP   VALUE EXCHANGE
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Value Before Swapping ");
+
+         System.out.println("a :"+a);
+         System.out.println("b :" +b);
+     
+        // for Swap
+         int temp = a;
+         a = b;
+         b = temp;
+
+        System.out.println("Value After Swapping ");
+
+         System.out.println("a :" +a);
+         System.out.print("b :" +b);
+
+    }
+}

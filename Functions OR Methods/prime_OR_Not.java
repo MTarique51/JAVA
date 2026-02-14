0b4c83c6e9a12390a64625cc76d306ad4f4f2eec
MTarique51 <mohdtarique7223@gmail.com>
2026-02-14 00:01:32 -1200

    Optimised Code

--- Functions OR Methods/prime_OR_Not.java
+++ Functions OR Methods/prime_OR_Not.java
@@ -1,19 +1,38 @@
 public class prime_OR_Not {
 
+    //Prime OR Not //
     public static boolean isPrime(int n){
         boolean isPrime = true;
-        if(n == 2){
+    //     if(n == 2){
+    //         return true;
+    //     }
+
+    //     for(int i =2; i<=n-1; i++){
+    //         if(n%i == 0){
+    //              isPrime = false;
+    //         }
+    //     }
+    //     return isPrime;
+    // }
+    // public static void main(String[] args) {
+    //     System.out.println("Prime : " +isPrime(2));
+    // }
+
+    // Prime Or Not (Optimised)//
+
+     if(n == 2){
             return true;
         }
 
         for(int i =2; i<=n-1; i++){
             if(n%i == 0){
-                 isPrime = false;
+                 return false;
             }
         }
-        return isPrime;
+        return true;
     }
     public static void main(String[] args) {
         System.out.println("Prime : " +isPrime(2));
     }
 }
+ 

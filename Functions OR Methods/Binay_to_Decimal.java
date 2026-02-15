dff6e2b1d74add6739f49dedc9f137fd424055c0
MTarique51 <mohdtarique7223@gmail.com>
2026-02-15 11:44:25 -1200

    Add decimal to Binary

--- Functions OR Methods/Binay_to_Decimal.java
+++ Functions OR Methods/Binay_to_Decimal.java
@@ -12,9 +12,26 @@ public class Binay_to_Decimal {
             pow++;
             binNum=binNum/10;
         }
-        System.out.println("Decimal of" +myNum+"="+decNum);
+        System.out.println("Decimal of " +myNum+"= "+decNum);
     }
+
+    public static void decToBin(int n){
+        int my_Num=n;
+        int pow=0;
+        int binNum=0;
+
+        while (n>0) {
+            int rem = n%2;
+            binNum= binNum+(rem*(int)Math.pow(10,pow));
+
+            pow++;
+            n=n/2;
+        }
+        System.out.println("Binary Form of " +my_Num+"= "+binNum);
+    }
+
     public static void main(String[] args) {
         binToDec(101);
+        decToBin(10);
     }
 }

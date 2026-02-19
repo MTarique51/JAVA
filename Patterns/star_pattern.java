73de1294c9eff11f664fb10d1b663c83c50ef08c
MTarique51 <mohdtarique7223@gmail.com>
2026-02-19 00:48:02 -1200

    Modified

--- Patterns/star_pattern.java
+++ Patterns/star_pattern.java
@@ -1,9 +1,9 @@
 public class star_pattern {
     public static void main(String[] args) {
 
-        for(int line = 1; line <= 16; line++){
+        for(int line = 1; line <= 10; line++){
             for(int star = 1; star <= line; star++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }

fced80452ee749cacd1b318c981799ec5a2561c2
MTarique51 <mohdtarique7223@gmail.com>
2026-03-08 01:34:20 -1200

    Arrays as Function Argument

---
+++ Array & Sorting/arrays_as_function_argument.java
@@ -0,0 +1,25 @@
+public class arrays_as_function_argument
+ {
+
+    public static void update(int marks[])
+    {
+        for(int i = 0; i<marks.length ; i++)
+        {
+            marks[i] = marks[i] +1 ;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int marks[] = {97,98,99};
+        update(marks);
+    
+        // This is for printing the Number //
+        for(int i = 0; i<marks.length; i++)
+        {
+            System.out.println(marks[i] + " ");
+        }
+
+        System.out.println();
+
+    }
+}

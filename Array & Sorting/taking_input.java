6c52780d28041de81e830f886820785af9ab6f67
MTarique51 <mohdtarique7223@gmail.com>
2026-03-08 01:03:37 -1200

    modified

--- Array & Sorting/taking_input.java
+++ Array & Sorting/taking_input.java
@@ -16,5 +16,10 @@ public class taking_input {
         System.out.println("Number of Phy : " + marks[0]);
         System.out.println("Number of Che : " + marks[1]);
         System.out.println("Number of Math : " + marks[2]);
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("Percentage  = " + percentage + "%");
+
+        System.out.println("Length of the Array  : " + marks.length);
     }
 }

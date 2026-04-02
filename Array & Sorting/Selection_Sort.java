dddd3e753449320030be94170ef6096194244331
MTarique51 <mohdtarique7223@gmail.com>
2026-04-01 22:02:53 -1200

    Add Insertion Sort Code

--- Array & Sorting/Selection_Sort.java
+++ Array & Sorting/Selection_Sort.java
@@ -47,11 +47,30 @@ public class Selection_Sort
             arr[i] = temp;
         }
     }
+
+    public static void insertionSort(int arr[])
+     {
+        for(int i=0 ; i<arr.length-1 ; i++)
+        {
+            int curr = arr[i];
+            int prev = i-1;
+
+            // Finding out the currect Position to insert
+            while(prev >= 0 && arr[prev] > curr)
+            {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+
+            // insertion 
+            arr[prev+1] = curr;
+        }
+     }
     public static void main(String[] args) 
     {
         int arr[] = {5 , 4 , 1 , 3 , 2};
-        System.out.print("Sorted Array According To The Selection sort : ");
-        selectionSort(arr);
+        System.out.print("Sorted Array According To : ");
+        insertionSort(arr);
         printArr(arr);
     }
 }

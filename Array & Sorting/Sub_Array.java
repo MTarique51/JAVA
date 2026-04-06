765ece2e2e3a291d0f5a5ad04e2dc237ce1cf57d
MTarique51 <mohdtarique7223@gmail.com>
2026-04-05 12:07:57 -1200

    Sub Array

---
+++ Array & Sorting/Sub_Array.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Sub_Array {
+
+    public static void subArray(int numbers[])
+    {
+        // int ts = 0;
+
+        for(int i=0 ; i<numbers.length ; i++)
+        {
+            int start = i;
+
+            for(int j=i ; j<numbers.length ; j++)
+            {
+                int end = j;
+
+                for(int k=start ; k<=end ; k++)               // print
+                {
+                    System.out.print(numbers[k] +" ");      // Sub Array
+                }
+                // ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        // System.out.println("Total Sub Arrays = " + ts);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2 , 4 , 6 , 8 , 10};
+        subArray(numbers);
+        
+    }
+}

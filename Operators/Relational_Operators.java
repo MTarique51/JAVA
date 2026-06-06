4c81560e4454204c5d3e75c8f2897767b8879eb0
MTarique51 <mohdtarique7223@gmail.com>
2026-06-06 09:12:09 -1200

    Updation in value

--- Operators/Relational_Operators.java
+++ Operators/Relational_Operators.java
@@ -1,8 +1,8 @@
 public class Relational_Operators {
     public static void main(String[] args) {
-        int A = 15;
-        int B = 10;
-        int C = 15;
+        int A = 30;
+        int B = 20;
+        int C = 30;
         System.out.println("According to comparision :" +  (A == B));                     // USE OF == 
         System.out.println("According to Unequal :" + (A != B));                         // USE OF !=
         System.out.println("According to Greater Than :" + (A > B));                     // USE OF < 
